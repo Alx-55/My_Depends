15075b0a889278b78cb1ef14e1e4fab65e28dd47
Alexander <olexandrhai115@gmail.com>
2025-05-08 16:11:38 +0300

    Добавление "ручек" в проект

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
@@ -50,7 +50,7 @@ async def setup_database():
         await conn.run_sync(Base.metadata.create_all)
 
 
-@app.post("/books")
+@app.post("/books")  # добавление книги
 async def add_book(book: BookSchema, session: SessionDep) -> BookSchema:
     new_book = BookModel(
         title=book.title,
@@ -66,14 +66,14 @@ from pydantic import BaseModel, Field
 
 class PaginationParams(BaseModel):  # Параметры пагинации. Пагинация - возвращение результатов с сервера не сразу, а частями; чтобы получить
                                     # нужный результат, в запросе нужно указать параметры.
-    limit: int = Field(10, ge=1, le=100, description="Количество элементов на странице")
-    offset: int = Field(0, ge=0, description="Смещение для пагинации")  # offset - это как-бы сдвиг при формировании результата запроса
+    limit: int = Field(20, ge=1, le=100, description="Количество элементов на странице")
+    offset: int = Field(0, ge=0, description="Смещение для пагинации")  # offset - это как-бы 'сдвиг' при формировании результата запроса
 
 
 PaginationDep = Annotated[PaginationParams, Depends(PaginationParams)]
 
 
-@app.get("/books")
+@app.get("/books") # получение книги/книг из БД (параметры пагинации задаются по умолчанию или же может задавать сам клиент)
 async def get_books(session: SessionDep, pagination: PaginationDep) -> list[BookGetSchema]:
     query = (
         select(BookModel)
@@ -82,3 +82,37 @@ async def get_books(session: SessionDep, pagination: PaginationDep) -> list[Book
     )
     result = await session.execute((query))
     return result.scalars().all()
+
+
+@app.delete("/books/{book_id}")  # удаление ненужной книги по id-шнику
+async def delete_book(book_id: int, session: SessionDep):
+    book = await session.get(BookModel, book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Книга не найдена")
+
+    await session.delete(book)
+    await session.commit()
+    return {"detail": f"Книга с id={book_id} удалена"}
+
+
+# обновим модель запроса (сделаем поля опциональными):
+class BookUpdateSchema(BaseModel):
+    title: str | None = None
+    author: str | None = None
+
+
+@app.put("/books/{book_id}", response_model=BookGetSchema)  # "ручка" обновления (редактирование книги)
+                                                                 # Позволяет клиенту обновить title и author у существующей записи по id.
+async def update_book(book_id: int, book_data: BookUpdateSchema, session: SessionDep):
+    book = await session.get(BookModel, book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Книга не найдена")
+
+    if book_data.title is not None:
+        book.title = book_data.title
+    if book_data.author is not None:
+        book.author = book_data.author
+
+    await session.commit()
+    await session.refresh(book)  # Обновим объект после коммита
+    return book
